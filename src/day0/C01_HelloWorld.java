2c13095cf47aab3e20e843b9b2edf0fcb6a38803
Ali AKPINAR <ali2.akpinar.42@gmail.com>
2022-11-03 14:08:03 +0100

    kemal son version

---
+++ src/day0/C01_HelloWorld.java
@@ -0,0 +1,22 @@
+package day0;
+
+public class C01_HelloWorld {
+
+    public static void main(String[] args) {
+
+        // IT dunyasina merhaba diyelim
+
+        System.out.println("Hello World");
+
+        /*
+        Birden fazla
+        satiri yorum haline
+        getirmek icin
+        bu yontem kullanilir
+         */
+
+    }
+
+
+}
+
